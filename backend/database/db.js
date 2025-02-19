f93ea471c43e3f872dee6bcc2e1c26d0d77a73e4
Resham <reshamupadhyay48@gmail.com>
2025-02-19 23:03:46 +0530

    db schema's

---
+++ backend/database/db.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Types.ObjectId;
+require('dotenv').config();
+
+
+const userSchema = new Schema({
+    email:{type:String, unique: true},
+    password:{type:String},
+    firstName: String,
+    lastName: String
+});
+
+const adminSchema = new Schema({
+    email:{type:String, unique: true},
+    password:{type:String},
+    firstName: String,
+    lastName: String
+});
+
+const courseSchema = new Schema({
+    title: String,
+    description:String,
+    price: Number,
+    imageUrl: String,
+    creatorId: {type:ObjectId, ref:"admin"}
+});
+
+const purchaseSchema = new Schema({
+    userId : {type:ObjectId, ref:'user'},
+    courseId : {type:ObjectId, ref:'course'}
+});
+
+
+const userModel = mongoose.model("user", userSchema);
+const adminModel = mongoose.model("admin", adminSchema);
+const courseModel = mongoose.model("course", courseSchema);
+const purchaseModel = mongoose.model("purchase", purchaseSchema);
+
+module.exports = {
+    userModel,
+    adminModel,
+    courseModel,
+    purchaseModel
+}
