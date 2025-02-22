c56dcb290723d6fd74aada393c9ac93c284973e7
Resham <reshamupadhyay48@gmail.com>
2025-02-22 22:36:31 +0530

    completed the user signup and login part

--- frontend/registration.js
+++ frontend/registration.js
@@ -1,23 +1,90 @@
-const loginForm = document.getElementById("loginForm");
-const signupForm = document.getElementById("signupForm");
+const loginContainer = document.getElementById("login-container");
+const signupContainer = document.getElementById("signup-container");
 const container = document.getElementsByClassName("wrapper");
 
-function toggleForm(formType){
-    if(formType === 'signup'){
-        loginForm.style.display = 'none';
-        signupForm.style.display = 'flex';
-        container.classList.add("signup-height");       
-    } else {
-        loginForm.style.display = 'flex';
-        signupForm.style.display = 'none';
-    }
+let isSigningUp = false;
+
+function toggleForm(formType) {
+  if (formType === "signup") {
+    loginContainer.style.display = "none";
+    signupContainer.style.display = "flex";
+  } else {
+    loginContainer.style.display = "flex";
+    signupContainer.style.display = "none";
+  }
 }
 
-document.querySelectorAll('.redirect-link').forEach(link => {
-    link.addEventListener('click', (e) => {
-        e.preventDefault();
-        console.log(e.target.dataset.form);
-        toggleForm(e.target.dataset.form);
-    });
+document.querySelectorAll(".redirect-link").forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    toggleForm(e.target.dataset.form);
+  });
 });
 
+const URL = "http://localhost:3000/user";
+
+const loginForm = document.getElementById("login-form");
+const signupForm = document.getElementById("signup-form");
+
+signupForm.addEventListener(
+  "submit",
+  async function (event) {
+    event.preventDefault();
+    if (isSigningUp) return;
+    isSigningUp = true;
+    console.log("signup clicked");
+
+    const firstName = document.getElementById("firstName");
+    const lastName = document.getElementById("lastName");
+    const email = document.getElementById("signup-email");
+    const password = document.getElementById("signup-password");
+
+    const bodyData = {
+      firstName: firstName.value,
+      lastName: lastName.value,
+      email: email.value,
+      password: password.value,
+    };
+    const endpoint = "signup";
+    const responseData = await postData(bodyData, endpoint);
+    console.log(responseData);
+  },
+  false
+);
+
+loginForm.addEventListener(
+  "submit",
+  async function (event) {
+    event.preventDefault();
+    console.log("login clicked");
+
+    const email = document.getElementById("login-email");
+    const password = document.getElementById("login-password");
+
+    const bodyData = {
+      email: email.value,
+      password: password.value,
+    };
+    const endpoint = "login";
+    console.log(endpoint);
+    const responseData = await postData(bodyData, endpoint);
+    const token = responseData.token;
+    localStorage.setItem("token", token);
+  },
+  false
+);
+
+async function postData(bodyData, endpoint) {
+  try {
+    const response = await fetch(`${URL}/${endpoint}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(bodyData),
+    });
+    return response.json();
+  } catch (error) {
+    console.log(error);
+  }
+}
